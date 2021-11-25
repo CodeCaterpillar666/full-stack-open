c0821e40e04c298ea7867de5abee1f251f7b2f53
ziweiwang98 <wang.ziwei2@northeastern.edu>
2021-11-24 23:48:23 -0800

    Complete Exercise 1.3.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,34 +3,24 @@ import './App.css';
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
-      {/* <h1>{course}</h1> */}
-      <Header course={course}/>
-
-      {/* <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p> */}
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-
-      {/* <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
-      <Footer exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-
-
-
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Footer part1={part1} part2={part2} part3={part3} />
     </div>
   )
 }
@@ -56,9 +46,9 @@ function Header(props) {
 function Content(props) {
   return (
     <>
-      <Part part1={props.part1} exercises1={props.exercises1}/>
-      <Part part1={props.part2} exercises1={props.exercises2}/>
-      <Part part1={props.part3} exercises1={props.exercises2}/>
+      <Part part1={props.part1.name} exercises1={props.part1.exercises}/>
+      <Part part1={props.part2.name} exercises1={props.part2.exercises}/>
+      <Part part1={props.part3.name} exercises1={props.part3.exercises}/>
     </>
   )
 }
@@ -66,7 +56,7 @@ function Content(props) {
 function Footer(props) {
   return (
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
     </>
   )
 }
